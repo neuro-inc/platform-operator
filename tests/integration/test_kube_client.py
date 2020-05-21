fcdfb69931c4bfed1fcefc3ef14f851a2f779f3b
Ivan <vanya.zubenko@neuromation.io>
2020-05-21 22:35:41 +0300

    add get pods to kube client

--- tests/integration/test_kube_client.py
+++ tests/integration/test_kube_client.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import aiohttp
 import pytest
 
@@ -38,3 +40,48 @@ class TestKubeClient:
     ) -> None:
         with pytest.raises(aiohttp.ClientError):
             await kube_client.get_secret(namespace="default", name="unknown")
+
+    @pytest.mark.asyncio
+    async def test_get_pods(self, kube_client: KubeClient) -> None:
+        result = await kube_client.get_pods(namespace="kube-system")
+
+        assert len(result) > 1
+
+    @pytest.mark.asyncio
+    async def test_get_pods_with_limit(self, kube_client: KubeClient) -> None:
+        result = await kube_client.get_pods(namespace="kube-system", limit=1)
+
+        assert len(result) == 1
+
+    @pytest.mark.asyncio
+    async def test_get_pods_with_label_selector(self, kube_client: KubeClient) -> None:
+        pods = await kube_client.get_pods(
+            namespace="kube-system", label_selector={"k8s-app": "kube-proxy"}
+        )
+
+        for pod in pods:
+            assert pod["metadata"]["name"].startswith("kube-proxy")
+
+    @pytest.mark.asyncio
+    async def test_get_pods_in_unknown_namespace(self, kube_client: KubeClient) -> None:
+        result = await kube_client.get_pods(namespace="unknown")
+
+        assert result == []
+
+    @pytest.mark.asyncio
+    async def test_wait_till_pods_deleted(
+        self, kube_client: KubeClient, kube_namespace: str
+    ) -> None:
+        await kube_client.wait_till_pods_deleted(namespace=kube_namespace)
+
+    @pytest.mark.asyncio
+    async def test_wait_till_pods_deleted_forever(
+        self, kube_client: KubeClient
+    ) -> None:
+        with pytest.raises(asyncio.TimeoutError):
+            await asyncio.wait_for(
+                kube_client.wait_till_pods_deleted(
+                    namespace="kube-system", interval_secs=1
+                ),
+                2,
+            )
