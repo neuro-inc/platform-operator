16309b8af5d9e75c6fab1abe48e932b255a2bc9f
Ivan <vanya.zubenko@neuromation.io>
2020-05-22 11:41:30 +0300

    add platform registry helm values

--- platform_operator/helm_values.py
+++ platform_operator/helm_values.py
@@ -4,6 +4,48 @@ from .models import PlatformConfig
 
 
 class HelmValuesFactory:
+    def create_platform_registry_values(
+        self, platform: PlatformConfig
+    ) -> Dict[str, Any]:
+        result: Dict[str, Any] = {
+            "INGRESS_HOST": platform.ingress_registry_url.host,
+            "NP_CLUSTER_NAME": platform.cluster_name,
+            "NP_REGISTRY_AUTH_URL": str(platform.auth_url),
+            "NP_REGISTRY_UPSTREAM_MAX_CATALOG_ENTRIES": 10000,
+            "DOCKER_LOGIN_ARTIFACTORY_SECRET_NAME": platform.image_pull_secret_name,
+        }
+        if platform.gcp:
+            result["NP_REGISTRY_UPSTREAM_TYPE"] = "oauth"
+            result["NP_REGISTRY_UPSTREAM_URL"] = "https://gcr.io"
+            result["NP_REGISTRY_UPSTREAM_PROJECT"] = platform.gcp.project
+        if platform.aws:
+            result["NP_REGISTRY_UPSTREAM_TYPE"] = "aws_ecr"
+            result["NP_REGISTRY_UPSTREAM_URL"] = str(platform.aws.registry_url)
+            result["NP_REGISTRY_UPSTREAM_PROJECT"] = "neuro"
+            result["NP_REGISTRY_UPSTREAM_MAX_CATALOG_ENTRIES"] = 1000
+            result["AWS_DEFAULT_REGION"] = platform.aws.region
+        if platform.azure:
+            result["NP_REGISTRY_UPSTREAM_TYPE"] = "oauth"
+            result["NP_REGISTRY_UPSTREAM_URL"] = str(platform.azure.registry_url)
+            result["NP_REGISTRY_UPSTREAM_PROJECT"] = "neuro"
+            result[
+                "NP_REGISTRY_UPSTREAM_TOKEN_SERVICE"
+            ] = platform.azure.registry_url.host
+            result["NP_REGISTRY_UPSTREAM_TOKEN_URL"] = str(
+                platform.azure.registry_url / "oauth2/token"
+            )
+        if platform.aws and platform.aws.role_ecr_arn:
+            result["annotations"] = {
+                "iam.amazonaws.com/role": platform.aws.role_ecr_arn
+            }
+        if platform.on_prem:
+            result["NP_REGISTRY_UPSTREAM_TYPE"] = "basic"
+            result[
+                "NP_REGISTRY_UPSTREAM_URL"
+            ] = f"http://{platform.namespace}-docker-registry:5000"
+            result["NP_REGISTRY_UPSTREAM_PROJECT"] = "neuro"
+        return result
+
     def create_platform_monitoring_values(
         self, platform: PlatformConfig
     ) -> Dict[str, Any]:
