c954661487e3df043bfa17c4860884cd68cec77f
Ivan Zubenko <vanya.zubenko@neuromation.io>
2020-05-22 11:44:55 +0300

    add platform registry helm values (#17)

--- tests/unit/test_helm_values.py
+++ tests/unit/test_helm_values.py
@@ -11,6 +11,93 @@ class TestHelmValuesFactory:
     def factory(self) -> HelmValuesFactory:
         return HelmValuesFactory()
 
+    def test_create_gcp_platform_registry_values(
+        self, gcp_platform_config: PlatformConfig, factory: HelmValuesFactory
+    ) -> None:
+        result = factory.create_platform_registry_values(gcp_platform_config)
+
+        assert result == {
+            "INGRESS_HOST": f"registry.{gcp_platform_config.cluster_name}.org.neu.ro",
+            "NP_CLUSTER_NAME": gcp_platform_config.cluster_name,
+            "NP_REGISTRY_AUTH_URL": "https://dev.neu.ro",
+            "NP_REGISTRY_UPSTREAM_MAX_CATALOG_ENTRIES": 10000,
+            "DOCKER_LOGIN_ARTIFACTORY_SECRET_NAME": "platform-docker-config",
+            "NP_REGISTRY_UPSTREAM_TYPE": "oauth",
+            "NP_REGISTRY_UPSTREAM_URL": "https://gcr.io",
+            "NP_REGISTRY_UPSTREAM_PROJECT": "project",
+        }
+
+    def test_create_aws_platform_registry_values(
+        self, aws_platform_config: PlatformConfig, factory: HelmValuesFactory
+    ) -> None:
+        result = factory.create_platform_registry_values(aws_platform_config)
+
+        assert result == {
+            "INGRESS_HOST": (f"registry.{aws_platform_config.cluster_name}.org.neu.ro"),
+            "NP_CLUSTER_NAME": aws_platform_config.cluster_name,
+            "NP_REGISTRY_AUTH_URL": "https://dev.neu.ro",
+            "NP_REGISTRY_UPSTREAM_MAX_CATALOG_ENTRIES": 1000,
+            "DOCKER_LOGIN_ARTIFACTORY_SECRET_NAME": "platform-docker-config",
+            "NP_REGISTRY_UPSTREAM_TYPE": "aws_ecr",
+            "NP_REGISTRY_UPSTREAM_URL": (
+                "https://platform.dkr.ecr.us-east-1.amazonaws.com"
+            ),
+            "NP_REGISTRY_UPSTREAM_PROJECT": "neuro",
+            "AWS_DEFAULT_REGION": "us-east-1",
+        }
+
+    def test_create_aws_platform_registry_values_with_role(
+        self, aws_platform_config: PlatformConfig, factory: HelmValuesFactory
+    ) -> None:
+        result = factory.create_platform_registry_values(
+            replace(
+                aws_platform_config,
+                aws=replace(aws_platform_config.aws, role_ecr_arn="ecr_role"),
+            )
+        )
+
+        assert result["annotations"] == {"iam.amazonaws.com/role": "ecr_role"}
+
+    def test_create_azure_platform_registry_values(
+        self, azure_platform_config: PlatformConfig, factory: HelmValuesFactory
+    ) -> None:
+        result = factory.create_platform_registry_values(azure_platform_config)
+
+        assert result == {
+            "INGRESS_HOST": (
+                f"registry.{azure_platform_config.cluster_name}.org.neu.ro"
+            ),
+            "NP_CLUSTER_NAME": azure_platform_config.cluster_name,
+            "NP_REGISTRY_AUTH_URL": "https://dev.neu.ro",
+            "NP_REGISTRY_UPSTREAM_MAX_CATALOG_ENTRIES": 10000,
+            "DOCKER_LOGIN_ARTIFACTORY_SECRET_NAME": "platform-docker-config",
+            "NP_REGISTRY_UPSTREAM_TYPE": "oauth",
+            "NP_REGISTRY_UPSTREAM_URL": "https://platform.azurecr.io",
+            "NP_REGISTRY_UPSTREAM_PROJECT": "neuro",
+            "NP_REGISTRY_UPSTREAM_TOKEN_SERVICE": "platform.azurecr.io",
+            "NP_REGISTRY_UPSTREAM_TOKEN_URL": (
+                "https://platform.azurecr.io/oauth2/token"
+            ),
+        }
+
+    def test_create_on_prem_platform_registry_values(
+        self, on_prem_platform_config: PlatformConfig, factory: HelmValuesFactory
+    ) -> None:
+        result = factory.create_platform_registry_values(on_prem_platform_config)
+
+        assert result == {
+            "INGRESS_HOST": (
+                f"registry.{on_prem_platform_config.cluster_name}.org.neu.ro"
+            ),
+            "NP_CLUSTER_NAME": on_prem_platform_config.cluster_name,
+            "NP_REGISTRY_AUTH_URL": "https://dev.neu.ro",
+            "NP_REGISTRY_UPSTREAM_MAX_CATALOG_ENTRIES": 10000,
+            "DOCKER_LOGIN_ARTIFACTORY_SECRET_NAME": "platform-docker-config",
+            "NP_REGISTRY_UPSTREAM_TYPE": "basic",
+            "NP_REGISTRY_UPSTREAM_URL": "http://platform-docker-registry:5000",
+            "NP_REGISTRY_UPSTREAM_PROJECT": "neuro",
+        }
+
     def test_create_platform_monitoring_values(
         self, gcp_platform_config: PlatformConfig, factory: HelmValuesFactory
     ) -> None:
