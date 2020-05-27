2c69d9fd416c79edcf607b7ab0e282a60dfc2bc6
Ivan Zubenko <vanya.zubenko@neuromation.io>
2020-05-27 16:14:00 +0300

    add platform deploy handler (#33)

--- platform_operator/handlers.py
+++ platform_operator/handlers.py
@@ -1,16 +1,26 @@
+import asyncio
 import logging
 from contextlib import AsyncExitStack
 from dataclasses import dataclass, field
+from logging import Logger
 from typing import Any, Dict, Optional
 
 import kopf
+from kopf.structs import bodies
 
 from .aws_client import AwsElbClient
 from .config_client import ConfigClient
 from .helm_client import HelmClient
 from .helm_values import HelmValuesFactory
-from .kube_client import KubeClient
-from .models import Config, PlatformConfig, PlatformConfigFactory
+from .kube_client import (
+    PLATFORM_API_VERSION,
+    PLATFORM_GROUP,
+    PLATFORM_PLURAL,
+    KubeClient,
+    PlatformConditionType,
+    PlatformStatusManager,
+)
+from .models import Config, HelmRepoName, PlatformConfig, PlatformConfigFactory
 
 
 @dataclass
@@ -58,6 +68,117 @@ async def cleanup(**_: Any) -> None:
     await app.close()
 
 
+@kopf.on.create(
+    PLATFORM_GROUP, PLATFORM_API_VERSION, PLATFORM_PLURAL, backoff=config.backoff
+)
+@kopf.on.update(
+    PLATFORM_GROUP, PLATFORM_API_VERSION, PLATFORM_PLURAL, backoff=config.backoff,
+)
+async def deploy(
+    name: str, body: bodies.Body, logger: Logger, retry: int, **kwargs: Any,
+) -> None:
+    status_manager = PlatformStatusManager(
+        app.kube_client, namespace=config.platform_namespace, name=name, logger=logger,
+    )
+    if retry > config.retries:
+        await status_manager.fail_deployment()
+        raise kopf.HandlerRetriesError(
+            f"Platform deployment has exceeded {config.retries} retries"
+        )
+    else:
+        await status_manager.start_deployment(retry)
+
+    platform_token = body["spec"]["token"]
+    cluster = await app.config_client.get_cluster(
+        cluster_name=name, token=platform_token
+    )
+
+    try:
+        platform = app.platform_config_factory.create(body, cluster)
+    except Exception as ex:
+        await status_manager.fail_deployment()
+        raise kopf.PermanentError(f"Invalid platform configuration: {ex!s}")
+
+    logger.info("Platform deployment started")
+
+    await app.helm_client.init(client_only=True, skip_refresh=True)
+    await app.helm_client.add_repo(config.helm_stable_repo)
+    await app.helm_client.add_repo(platform.helm_repo)
+    await app.helm_client.update_repo()
+
+    is_gcp_gcs_platform = platform.gcp and platform.gcp.storage_type == "gcs"
+    if is_gcp_gcs_platform and not status_manager.is_condition_satisfied(
+        PlatformConditionType.OBS_CSI_DRIVER_DEPLOYED
+    ):
+        async with status_manager.transition(
+            PlatformConditionType.OBS_CSI_DRIVER_DEPLOYED
+        ):
+            await app.helm_client.upgrade(
+                config.helm_release_names.obs_csi_driver,
+                f"{HelmRepoName.NEURO}/{config.helm_chart_names.obs_csi_driver}",
+                values=app.helm_values_factory.create_obs_csi_driver_values(platform),
+                version=config.helm_chart_versions.obs_csi_driver,
+                namespace=config.platform_namespace,
+                install=True,
+                wait=True,
+                timeout=600,
+            )
+
+    if platform.on_prem and not status_manager.is_condition_satisfied(
+        PlatformConditionType.NFS_SERVER_DEPLOYED
+    ):
+        async with status_manager.transition(PlatformConditionType.NFS_SERVER_DEPLOYED):
+            await app.helm_client.upgrade(
+                config.helm_release_names.nfs_server,
+                f"{HelmRepoName.NEURO}/{config.helm_chart_names.nfs_server}",
+                values=app.helm_values_factory.create_nfs_server_values(platform),
+                version=config.helm_chart_versions.nfs_server,
+                namespace=config.platform_namespace,
+                install=True,
+                wait=True,
+                timeout=600,
+            )
+
+    if not status_manager.is_condition_satisfied(
+        PlatformConditionType.PLATFORM_DEPLOYED
+    ):
+        async with status_manager.transition(PlatformConditionType.PLATFORM_DEPLOYED):
+            await app.helm_client.upgrade(
+                config.helm_release_names.platform,
+                f"{HelmRepoName.NEURO}/{config.helm_chart_names.platform}",
+                values=app.helm_values_factory.create_platform_values(platform),
+                version=config.helm_chart_versions.platform,
+                namespace=config.platform_namespace,
+                install=True,
+                wait=True,
+                timeout=600,
+            )
+
+    if not status_manager.is_condition_satisfied(
+        PlatformConditionType.SSL_CERT_CREATED
+    ):
+        async with status_manager.transition(PlatformConditionType.SSL_CERT_CREATED):
+            await wait_till_ssl_cert_created()
+
+    if not status_manager.is_condition_satisfied(PlatformConditionType.DNS_CONFIGURED):
+        async with status_manager.transition(PlatformConditionType.DNS_CONFIGURED):
+            await configure_dns(platform)
+
+    if not status_manager.is_condition_satisfied(
+        PlatformConditionType.CLUSTER_CONFIGURED
+    ):
+        async with status_manager.transition(PlatformConditionType.CLUSTER_CONFIGURED):
+            await configure_cluster(platform)
+
+    await status_manager.complete_deployment()
+    logger.info("Platform deployment succeeded")
+
+
+async def wait_till_ssl_cert_created() -> None:
+    # TODO: implement something smarter than just delay
+    await asyncio.sleep(300)
+
+
 async def configure_dns(platform: PlatformConfig) -> None:
     traefik_service = await app.kube_client.get_service(
         namespace=platform.namespace, name=platform.service_traefik_name
@@ -68,7 +189,6 @@ async def configure_dns(platform: PlatformConfig) -> None:
     aws_traefik_lb: Optional[Dict[str, Any]] = None
     aws_ssh_auth_lb: Optional[Dict[str, Any]] = None
     if platform.aws:
-        print(AwsElbClient)
         async with AwsElbClient(region=platform.aws.region) as client:
             aws_traefik_lb = await client.get_load_balancer_by_dns_name(
                 traefik_service["status"]["loadBalancer"]["ingress"][0]["hostname"]
