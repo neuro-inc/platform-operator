b33353fae4b2583229df2527ea5d64d476d09082
Ivan <vanya.zubenko@neuromation.io>
2020-06-16 10:23:36 +0300

    move platform helm chart from platform-infra repo

---
+++ deploy/platform/files/resolve_dns_challenge.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+set -o errexit
+set -o pipefail
+
+function _error {
+    echo "$1"
+    return 1
+}
+
+function _retry {
+    set +o errexit
+
+    MAX_RETRIES="$1"
+    RETRY_INTERVAL="$2"
+    N=0
+
+    shift
+    shift
+
+    while [ $N -lt "$MAX_RETRIES" ]; do
+        LAST_RESULT="$("${@}")"
+        RETURN_CODE=$?
+
+        if [ "$RETURN_CODE" == 0 ]; then
+            break
+        fi
+
+        N=$((N + 1))
+        sleep "$RETRY_INTERVAL"
+    done
+
+    set -o errexit
+
+    if [ ! -z "$LAST_RESULT" ]; then
+        echo "$LAST_RESULT"
+    fi
+
+    return ${RETURN_CODE:-1}
+}
+
+function _curl {
+    RESPONSE_FILE="$(mktemp "/tmp/response.XXXXXX")"
+    HTTP_CODE="$(curl -sL -o "$RESPONSE_FILE" -w "%{http_code}" "$@")"
+
+    if [ "$HTTP_CODE" -ge "400" ]; then
+        echo "http code: $HTTP_CODE, response: $(cat "$RESPONSE_FILE")"
+        RETURN_CODE=1
+    else
+        RETURN_CODE=0
+    fi
+
+    rm "$RESPONSE_FILE"
+
+    return ${RETURN_CODE:-1}
+}
+
+if [ -z "$NP_PLATFORM_API_URL" ]; then _error "NP_PLATFORM_API_URL is empty"; fi
+if [ -z "$NP_CLUSTER_TOKEN" ]; then _error "NP_CLUSTER_TOKEN is empty"; fi
+if [ -z "$NP_CLUSTER_NAME" ]; then _error "NP_CLUSTER_NAME is empty"; fi
+
+apk add -q --update --no-cache curl
+
+ACTION="$1"
+DNS_NAME="$2"
+VALUE="$3"
+PLATFORM_API_PATH="clusters/$NP_CLUSTER_NAME/lb/http/acme/challenges/dns"
+PAYLOAD="$(cat <<EOM
+{
+    "dns_name": "$DNS_NAME",
+    "value": "$VALUE"
+}
+EOM
+)"
+
+if [ "$ACTION" == "present" ]; then
+    _retry \
+        3 \
+        1 \
+        _curl -X PUT \
+            -H "Authorization: Bearer $NP_CLUSTER_TOKEN" \
+            -d "$PAYLOAD" \
+            "$NP_PLATFORM_API_URL/$PLATFORM_API_PATH"
+fi
+
+if [ "$ACTION" == "cleanup" ]; then
+    _retry \
+        3 \
+        1 \
+        _curl -X DELETE \
+            -H "Authorization: Bearer $NP_CLUSTER_TOKEN" \
+            -d "$PAYLOAD" \
+            "$NP_PLATFORM_API_URL/$PLATFORM_API_PATH"
+fi
---
+++ deploy/platform/templates/_helpers.tpl
@@ -0,0 +1,70 @@
+{{- define "platform.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{- define "platform.fullname" -}}
+{{- $name := default .Chart.Name .Values.nameOverride -}}
+{{- if contains $name .Release.Name -}}
+{{- .Release.Name | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+{{- end -}}
+
+{{- define "platform.chart" -}}
+{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" -}}
+{{- end -}}
+
+{{- define "platform.labels.standard" -}}
+app: {{ include "platform.name" . }}
+chart: {{ include "platform.chart" . }}
+heritage: {{ .Release.Service | quote }}
+release: {{ .Release.Name | quote }}
+{{- end -}}
+
+{{- define "platform.azure.storageAccount.secretName" -}}
+{{ .Release.Name }}-azure-storage-account
+{{- end -}}
+
+{{- define "platform.jobs.serviceAccountName" -}}
+{{ .Release.Name }}-jobs
+{{- end -}}
+
+{{- define "platform.jobs.namespace.name" -}}
+{{- if .Values.jobs.namespace.name -}}
+{{- .Values.jobs.namespace.name | quote -}}
+{{- else -}}
+{{- printf "%s-jobs" .Release.Namespace | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+{{- end -}}
+
+{{- define "platform.idleJobs.priorityClass.name" -}}
+{{ .Release.Name }}-idle-job
+{{- end -}}
+
+{{- define "platform.storage.platformPvName" -}}
+{{ .Release.Name }}-storage
+{{- end -}}
+
+{{- define "platform.storage.jobsPvName" -}}
+{{ .Release.Name }}-jobs-storage
+{{- end -}}
+
+{{- define "platform.dockerConfigJson" -}}
+{
+  "auths": {
+    {{ .url | quote }}: {
+        "username": {{ .username | quote }},
+        "password": {{ .password | quote }},
+        "email": {{ .email | quote }},
+        "auth": {{ printf "%s:%s" .username .password | b64enc | quote }}
+    }
+  }
+}
+{{- end -}}
+
+{{- define "platform.isSelfContained" -}}
+{{- if has true (values .Values.tags) -}}
+true
+{{- end -}}
+{{- end -}}
