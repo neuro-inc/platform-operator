a9a0f9423a7bed22037445098f251870aeb9a02c
Ivan Zubenko <vanya.zubenko@gmail.com>
2021-09-20 10:11:21 +0300

    fix minio image name

--- platform_operator/helm_values.py
+++ platform_operator/helm_values.py
@@ -284,7 +284,7 @@ class HelmValuesFactory:
         return {
             "image": {
                 "repository": f"{platform.docker_registry.url.host}/minio/minio",
-                "tag": "RELEASE.2021-08-25T00-41-18Z ",
+                "tag": "RELEASE.2021-08-25T00-41-18Z",
             },
             "imagePullSecrets": [
                 {"name": name} for name in platform.image_pull_secret_names
