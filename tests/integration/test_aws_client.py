305364086491e9c5cc76db3c8a7def8d40e5597d
Ivan Zubenko <vanya.zubenko@gmail.com>
2023-04-04 14:07:57 +0300

    use AWS elbv2 api for load balancers (#643)
    Co-authored-by: Ivan Zubenko <ivan.zubenko@angloamerican.com>

--- tests/integration/test_aws_client.py
+++ tests/integration/test_aws_client.py
@@ -4,6 +4,7 @@ import uuid
 from collections.abc import AsyncIterator
 from typing import Any
 
+import aiobotocore.session
 import pytest
 
 from platform_operator.aws_client import AwsElbClient
@@ -12,22 +13,27 @@ from platform_operator.aws_client import AwsElbClient
 class TestAwsElbClient:
     @pytest.fixture
     async def load_balancer(
-        self, elb_client: AwsElbClient
+        self, elb_client: AwsElbClient, ec2_client: aiobotocore.session.AioBaseClient
     ) -> AsyncIterator[dict[str, Any]]:
+        response = await ec2_client.create_vpc(CidrBlock="10.0.0.0/16")
+        response = await ec2_client.create_subnet(
+            VpcId=response["Vpc"]["VpcId"], CidrBlock="10.0.0.0/24"
+        )
         name = str(uuid.uuid4())
         response = await elb_client.create_load_balancer(
-            LoadBalancerName=name,
-            Listeners=[
-                {
-                    "Protocol": "string",
-                    "LoadBalancerPort": 123,
-                    "InstanceProtocol": "string",
-                    "InstancePort": 123,
-                }
-            ],
+            Name=name,
+            Type="network",
+            Scheme="internet-facing",
+            Subnets=[response["Subnet"]["SubnetId"]],
         )
-        yield {"LoadBalancerName": name, "DNSName": response["DNSName"]}
-        await elb_client.delete_load_balancer(LoadBalancerName=name)
+        load_balancer = response["LoadBalancers"][0]
+        yield {
+            "LoadBalancerName": name,
+            "DNSName": load_balancer["DNSName"],
+        }
+        await elb_client.delete_load_balancer(
+            LoadBalancerArn=load_balancer["LoadBalancerArn"]
+        )
 
     async def test_get_load_balancer_by_dns_name(
         self, load_balancer: dict[str, Any], elb_client: AwsElbClient
