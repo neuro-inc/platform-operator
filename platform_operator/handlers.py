b447533a31868b53ee3c5b490369b25addf6c50a
Ivan Zubenko <vanya.zubenko@gmail.com>
2025-09-14 11:27:09 +0200

    remove unused platform status conditions

--- platform_operator/handlers.py
+++ platform_operator/handlers.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import asyncio
 import logging
-import ssl
 from collections.abc import AsyncIterator
 from contextlib import AsyncExitStack, asynccontextmanager
 from dataclasses import dataclass, field
@@ -12,7 +11,6 @@ from typing import Any
 import aiohttp
 import kopf
 from neuro_config_client import (
-    ACMEEnvironment,
     Cluster,
     ConfigClient,
     PatchClusterRequest,
@@ -27,7 +25,6 @@ from .kube_client import (
     PLATFORM_GROUP,
     PLATFORM_PLURAL,
     KubeClient,
-    PlatformConditionType,
     PlatformPhase,
     PlatformStatusManager,
 )
@@ -40,7 +37,7 @@ from .models import (
 )
 
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 @dataclass
@@ -51,7 +48,6 @@ class App:
     kube_client: KubeClient = None  # type: ignore
     status_manager: PlatformStatusManager = None  # type: ignore
     config_client: ConfigClient = None  # type: ignore
-    raw_client: aiohttp.ClientSession = None  # type: ignore
     exit_stack: AsyncExitStack = field(default_factory=AsyncExitStack)
 
     async def close(self) -> None:
@@ -76,7 +72,6 @@ async def startup(settings: kopf.OperatorSettings, **_: Any) -> None:
     app.config_client = await app.exit_stack.enter_async_context(
         ConfigClient(config.platform_config_url)
     )
-    app.raw_client = await app.exit_stack.enter_async_context(aiohttp.ClientSession())
 
     settings.posting.level = logging.getLevelName(config.log_level)
     settings.persistence.progress_storage = kopf.AnnotationsProgressStorage()
@@ -137,8 +132,6 @@ async def _deploy(name: str, body: kopf.Body, logger: kopf.Logger, retry: int) -
     try:
         cluster = await get_cluster(name, body)
         platform = app.platform_config_factory.create(body, cluster)
-    except asyncio.CancelledError:
-        raise
     except aiohttp.ClientError:
         raise
     except Exception as ex:
@@ -155,27 +148,28 @@ async def _deploy(name: str, body: kopf.Body, logger: kopf.Logger, retry: int) -
     platform_deploy_required = await is_platform_deploy_required(platform, install=True)
 
     if phase == PlatformPhase.DEPLOYED and not platform_deploy_required:
-        logger.info("Platform config didn't change, skipping platform deployment")
+        LOGGER.info("Platform config didn't change, skipping platform deployment")
         return
 
     logger.info("Platform deployment started")
 
     await app.status_manager.start_deployment(platform.cluster_name, retry)
 
-    if platform_deploy_required:
-        await upgrade_platform_helm_release(platform)
+    try:
+        if platform_deploy_required:
+            await upgrade_platform_helm_release(platform)
 
-    logger.info("Configuring cluster")
-    await configure_cluster(platform)
-    logger.info("Cluster configured")
+        LOGGER.info("Configuring cluster")
+        await configure_cluster(platform)
+        LOGGER.info("Cluster configured")
 
-    logger.info("Waiting for certificate")
-    await wait_for_certificate_created(platform)
-    logger.info("Certificate is ready")
+        await complete_deployment(cluster, platform)
 
-    await complete_deployment(cluster, platform)
-
-    logger.info("Platform deployment succeeded")
+        logger.info("Platform deployment completed")
+    except Exception as exc:
+        logger.error("Platform deployment failed: %s", exc)
+        LOGGER.exception("Platform deployment failed")
+        raise kopf.TemporaryError(str(exc), delay=config.backoff)
 
 
 @kopf.on.delete(
@@ -196,57 +190,13 @@ async def delete(
         ttl_s=15 * 60,
         sleep_s=3,
     ):
-        await _delete(name, body, logger)
-
-
-async def _delete(name: str, body: kopf.Body, logger: kopf.Logger) -> None:
-    try:
-        cluster = await get_cluster(name, body)
-        platform = app.platform_config_factory.create(body, cluster)
-    except aiohttp.ClientError:
-        raise
-    except asyncio.CancelledError:
-        raise
-    except Exception:
-        # If platform has invalid configuration than there was no deployment
-        # and no resources to delete. Platform resource can be safely deleted.
-        logger.warning(
-            "Platform has invalid configuration, delete handler cannot proceed"
-        )
-        return
-
-    await app.helm_client.delete(config.helm_release_names.platform, wait=True)
-
-    try:
-        # We need first to delete all pods that use storage.
-        # Otherwise they can stuck in Termnating state because kubernetes
-        # will fail to unmount volumes from pods.
-        logger.info("Waiting for job pods to be deleted")
-        await asyncio.wait_for(
-            app.kube_client.wait_till_pods_deleted(namespace=platform.jobs_namespace),
-            600,
-        )
-        logger.info("Job pods deleted")
-
-        logger.info("Waiting for platform storage pods to be deleted")
-        await asyncio.wait_for(
-            app.kube_client.wait_till_pods_deleted(
-                namespace=platform.namespace,
-                label_selector={"service": "platform-storage"},
-            ),
-            600,
-        )
-        logger.info("Platform storage pods deleted")
-    except TimeoutError:
-        message = "Timeout error while wating for pods to be deleted"
-        logger.error(message)
-        raise kopf.TemporaryError(message)
+        await app.helm_client.delete(config.helm_release_names.platform, wait=True)
 
 
 @kopf.on.daemon(
     PLATFORM_GROUP, PLATFORM_API_VERSION, PLATFORM_PLURAL, backoff=config.backoff
 )
-async def watch_config(
+async def watch(
     name: str | None,
     body: kopf.Body,
     stopped: kopf.DaemonStopped,
@@ -255,7 +205,7 @@ async def watch_config(
 ) -> None:
     assert name, "Platform resource name is required"
 
-    logger.info("Started watching platform config")
+    LOGGER.info("Started watching platform config")
 
     while True:
         # Async daemons do not need the `stopped` signal.
@@ -264,7 +214,7 @@ async def watch_config(
         if stopped:
             break
 
-        logger.info(
+        LOGGER.info(
             "Platform config will be checked in %d seconds",
             config.platform_config_watch_interval_s,
         )
@@ -280,21 +230,21 @@ async def watch_config(
             ):
                 await _update(name, body, logger)
         except asyncio.CancelledError:
-            logger.info("Stopped watching platform config")
+            LOGGER.info("Stopped watching platform config")
             raise
         except Exception as exc:
-            logger.warning("Watch iteration failed", exc_info=exc)
+            LOGGER.warning("Watch iteration failed", exc_info=exc)
 
 
 async def _update(name: str, body: kopf.Body, logger: kopf.Logger) -> None:
     phase = await app.status_manager.get_phase(name)
 
     if phase == PlatformPhase.PENDING:
-        logger.info("Platform has not been installed yet, nothing to update")
+        LOGGER.info("Platform has not been installed yet, nothing to update")
         return
 
     if phase == PlatformPhase.DEPLOYING or phase == PlatformPhase.DELETING:
-        logger.info("Cannot update platform while it is in %s phase", phase.value)
+        LOGGER.info("Cannot update platform while it is in %s phase", phase.value)
         return
 
     cluster = await get_cluster(name, body)
@@ -303,16 +253,16 @@ async def _update(name: str, body: kopf.Body, logger: kopf.Logger) -> None:
 
     if platform_deploy_failed:
         await app.status_manager.fail_deployment(name)
-        logger.warning("Platform helm release failed, skipping platform config update")
+        logger.warning("Platform helm release failed, skipping platform update")
         return
 
     platform_deploy_required = await is_platform_deploy_required(platform)
 
     if phase == PlatformPhase.DEPLOYED and not platform_deploy_required:
-        logger.info("Platform config didn't change, skipping platform config update")
+        LOGGER.info("Platform config didn't change, skipping platform update")
         return
 
-    logger.info("Platform config update started")
+    logger.info("Platform deployment started")
 
     await app.status_manager.start_deployment(platform.cluster_name)
 
@@ -320,21 +270,13 @@ async def _update(name: str, body: kopf.Body, logger: kopf.Logger) -> None:
         if platform_deploy_required:
             await upgrade_platform_helm_release(platform)
 
-        logger.info("Configuring cluster")
         await configure_cluster(platform)
-        logger.info("Cluster configured")
-
-        logger.info("Waiting for certificate")
-        await wait_for_certificate_created(platform)
-        logger.info("Certificate is ready")
-
         await complete_deployment(cluster, platform)
 
-        logger.info("Platform config update succeeded")
-    except asyncio.CancelledError:
-        raise
+        logger.info("Platform deployment completed")
     except Exception as exc:
-        logger.error("Platform config update failed", exc_info=exc)
+        logger.error("Platform deployment failed: %s", exc)
+        LOGGER.exception("Platform deployment failed")
         await app.status_manager.fail_deployment(name)
 
 
@@ -451,64 +393,26 @@ async def complete_deployment(cluster: Cluster, platform: PlatformConfig) -> Non
 
 
 async def upgrade_platform_helm_release(platform: PlatformConfig) -> None:
-    async with app.status_manager.transition(
-        platform.cluster_name, PlatformConditionType.PLATFORM_DEPLOYED
-    ):
-        await app.kube_client.update_service_account(
-            namespace=platform.namespace,
-            name=platform.service_account_name,
-            annotations=platform.service_account_annotations,
-            image_pull_secrets=platform.image_pull_secret_names,
-        )
-        await app.helm_client.upgrade(
-            config.helm_release_names.platform,
-            str(platform.helm_repo.url / HelmChartNames.platform),
-            values=app.helm_values_factory.create_platform_values(platform),
-            version=config.helm_chart_versions.platform,
-            install=True,
-            wait=True,
-            timeout_s=600,
-            username=platform.helm_repo.username,
-            password=platform.helm_repo.password,
-        )
-
-
-async def wait_for_certificate_created(
-    platform: PlatformConfig, timeout_s: float = 5 * 60
-) -> None:
-    if not platform.ingress_controller_install or not platform.ingress_acme_enabled:
-        return
-
-    async with app.status_manager.transition(
-        platform.cluster_name, PlatformConditionType.CERTIFICATE_CREATED
-    ):
-        await asyncio.wait_for(_wait_for_certificate_created(platform), timeout_s)
-
-
-async def _wait_for_certificate_created(platform: PlatformConfig) -> None:
-    ssl_context: ssl.SSLContext | bool = False
-    if platform.ingress_acme_environment == ACMEEnvironment.STAGING:
-        ssl_context = ssl.create_default_context(cafile=config.acme_ca_staging_path)
-
-    while True:
-        try:
-            async with app.raw_client.get(platform.ingress_url, ssl=ssl_context):
-                return
-        except ssl.SSLError:
-            pass
-        except aiohttp.ClientError:
-            pass
-        await asyncio.sleep(5)
+    await app.kube_client.update_service_account(
+        namespace=platform.namespace,
+        name=platform.service_account_name,
+        annotations=platform.service_account_annotations,
+        image_pull_secrets=platform.image_pull_secret_names,
+    )
+    await app.helm_client.upgrade(
+        config.helm_release_names.platform,
+        str(platform.helm_repo.url / HelmChartNames.platform),
+        values=app.helm_values_factory.create_platform_values(platform),
+        version=config.helm_chart_versions.platform,
+        install=True,
+        wait=True,
+        timeout_s=600,
+        username=platform.helm_repo.username,
+        password=platform.helm_repo.password,
+    )
 
 
 async def configure_cluster(platform: PlatformConfig) -> None:
-    async with app.status_manager.transition(
-        platform.cluster_name, PlatformConditionType.CLUSTER_CONFIGURED
-    ):
-        await _configure_cluster(platform)
-
-
-async def _configure_cluster(platform: PlatformConfig) -> None:
     ingress_service: dict[str, Any] | None = None
     aws_ingress_lb: dict[str, Any] | None = None
 
